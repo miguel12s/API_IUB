8ed32a101e4538037c610dbc3ec662cd9d9089b1
miguel12s <87570624+miguel12s@users.noreply.github.com>
2024-03-08 19:22:36 -0500

    commit

--- config/db_config.py
+++ config/db_config.py
@@ -1,12 +1,11 @@
 import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
-        # host="roundhouse.proxy.rlwy.net",
-        host='localhost',
-        user="root",
-        password="",
-        database="bd_origin"
-        # password="CF4B2EG1h1Hf3h-3E4d46Ef5a-e1dfb4",
-        # database="railway",
-        # port="39493"
+        host="roundhouse.proxy.rlwy.net",
+        # host='localhost',
+        # user="root",
+        # password="",
+        password="CF4B2EG1h1Hf3h-3E4d46Ef5a-e1dfb4",
+        database="railway",
+        port="39493"
     )
