51e83364736b66e2c01a5e92f55db9a0b1a28b53
miguel12s <87570624+miguel12s@users.noreply.github.com>
2024-03-24 12:15:01 -0500

    pruebas con el front

--- config/db_config.py
+++ config/db_config.py
@@ -10,7 +10,11 @@ def get_db_connection():
             password="CF4B2EG1h1Hf3h-3E4d46Ef5a-e1dfb4",
             database="railway",
             port="39493",
-
+# host="localhost",
+#             user="root",
+#             password="",
+#             database="bd_origin",
+            
         
 
         )
